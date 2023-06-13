ea41016573a4e54aa57b80265a9713f32cd7ae35
FinoanaRandria <fitiavanarex4@gmail.com>
2023-06-13 16:35:03 +0300

    odif ui

--- src/mg/Calculatrice/finoana/Frame.java
+++ src/mg/Calculatrice/finoana/Frame.java
@@ -11,7 +11,7 @@ public class Frame extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
     public JTextField z1;
-    public JButton b1 ,b2, b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15 ,b16,b17,b18,b19 ,b20;
+    public JButton b1 ,b2, b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15 ,b16,b17,b18,b19,b20,b21,b22,b23,b24,b25;
 	public Frame(String title) {
 		Container c =getContentPane();
 		
@@ -28,7 +28,7 @@ public class Frame extends JFrame implements ActionListener{
 		 this.b9 = new JButton("4");
 		 this.b10 = new JButton("5");
 		 this.b11 = new JButton("6");
-		 this.b12 = new JButton("7");
+		 this.b12 = new JButton("/");
 		 this.b13 = new JButton("7");
 		 this.b14 = new JButton("8");
 		 this.b15 = new JButton("9");
@@ -36,7 +36,12 @@ public class Frame extends JFrame implements ActionListener{
 		 this.b17 = new JButton("0");
 		 this.b18 = new JButton(",");
 		 this.b19 = new JButton("+");
-		 this.b20 = new JButton("");
+		 this.b20 = new JButton("-");
+		 this.b21 = new JButton("C");
+		 this.b22 = new JButton("<-");
+		 this.b23 = new JButton("x10^");
+		 this.b24 = new JButton("√x");
+		 this.b25 = new JButton("=");
 		 
 		 this.z1.setBounds(10, 20, 480, 100);
 		 this.b1.setBounds(10, 220, 80, 50);
@@ -59,7 +64,11 @@ public class Frame extends JFrame implements ActionListener{
 		 this.b18.setBounds(100, 460, 80, 50);
 		 this.b19.setBounds(190, 460, 80, 50);
 		 this.b20.setBounds(280, 460, 80, 50);
-		 
+		 this.b21.setBounds(380, 220, 80, 50);
+		 this.b22.setBounds(380, 280, 80, 50);
+		 this.b23.setBounds(380, 340, 80, 50);
+		 this.b24.setBounds(380, 400, 80, 50);
+		 this.b25.setBounds(380, 460, 80, 50);
 		 
 		 setTitle(title);
 		 setSize(500,700);
@@ -70,7 +79,7 @@ public class Frame extends JFrame implements ActionListener{
 		 
 		 c.add(z1);c.add(b1);c.add(b1);c.add(b2);c.add(b3);c.add(b4);c.add(b5);c.add(b6);
 		 c.add(b7);c.add(b8);c.add(b9);c.add(b10);c.add(b11);c.add(b12);c.add(b13);c.add(b14);c.add(b15);
-		 c.add(b16);c.add(b17);c.add(b18);c.add(b19);c.add(b20);
+		 c.add(b16);c.add(b17);c.add(b18);c.add(b19);c.add(b20);c.add(b21);c.add(b22);c.add(b23);c.add(b24);c.add(b25);
 	}
 
 	@Override
